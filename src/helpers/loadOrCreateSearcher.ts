746264c55933d003d9c523ad449fde59749de2f0
Elyx0 <elyx00@gmail.com>
2023-06-16 19:04:00 +0200

    Searcher create helper

---
+++ src/helpers/loadOrCreateSearcher.ts
@@ -0,0 +1,23 @@
+import { Address, BigInt, Bytes, log } from '@graphprotocol/graph-ts';
+import { ZERO, ADDRESS_ZERO, ZERO_INT } from './common';
+import { Searcher } from "../../generated/schema";
+import { loadOrCreateGlobalStats } from './loadOrCreateGlobalStats';
+
+const INACTIVE = 'INACTIVE';
+
+export function loadOrCreateSearcher(searcherAddress: Bytes): Searcher {
+    let searcher = Searcher.load(searcherAddress);
+    if (!searcher) {
+        searcher = new Searcher(searcherAddress);
+        searcher.address = searcherAddress;
+        searcher.bundlesLanded = ZERO_INT;
+        searcher.totalTipped = ZERO;
+        searcher.lastBundleLandedTimestamp = ZERO_INT;
+        searcher.save();
+
+        const stats = loadOrCreateGlobalStats();
+        stats.totalUniqueSearchers = stats.totalUniqueSearchers.plus(BigInt.fromI32(1));
+        stats.save();
+    }
+    return searcher;
+}
